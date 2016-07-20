80e13a54dd54a801cbbdbd626a368570c2d4c8cd
ryanneely11 <ryanneely11@gmail.com>
2016-07-20 13:04:40 -0700

    added function to plot a logged session

--- log_parse.py
+++ log_parse.py
@@ -6,6 +6,11 @@
 ##Ryan Neely
 ##July 2016
 
+import numpy as np
+from scipy.ndimage.filters import gaussian_filter
+import matplotlib.pyplot as plt
+from matplotlib import gridspec
+
 """
 Takes in as an argument a log.txt files
 Returns a dictionary of arrays containing the 
@@ -64,3 +69,116 @@ def parse_log(f_in):
 		current_line = f.readline()
 	f.close()
 	return results
+
+
+def gauss_convolve(array, sigma):
+	"""
+	takes in an array with dimenstions samples x trials.
+	Returns an array of the same size where each trial is convolved with
+	a gaussian kernel with sigma = sigma.
+	"""
+	##remove singleton dimesions and make sure values are floats
+	array = array.squeeze().astype(float)
+	##allocate memory for result
+	result = np.zeros(array.shape)
+	##if the array is 2-D, handle each trial separately
+	try:
+		for trial in range(array.shape[1]):
+			result[:,trial] = gaussian_filter(array[:, trial], sigma = sigma, order = 0, mode = "constant", cval = 0.0)
+	##if it's 1-D:
+	except IndexError:
+		if array.shape[0] == array.size:
+			result = gaussian_filter(array, sigma = sigma, order = 0, mode = "constant", cval = 0.0)
+		else:
+			print "Check your array dimenszions!"
+	return result
+
+
+"""
+takes in a data dictionary produced by parse_log
+plots the lever presses and the switch points for levers
+"""
+def plot_presses(data_dict, sigma = 90):
+	##extract relevant data
+	top = data_dict['top_lever']
+	bottom = data_dict['bottom_lever']
+	duration = np.ceil(data_dict['session_length'])
+	top_rewarded = np.asarray(data_dict['top_rewarded'])/60.0
+	bottom_rewarded = np.asarray(data_dict['bottom_rewarded'])/60.0
+	##convert timestamps to histogram structures
+	top, edges = np.histogram(top, bins = duration)
+	bottom, edges = np.histogram(bottom, bins = duration)
+	##smooth with a gaussian window
+	top = gauss_convolve(top, sigma)
+	bottom = gauss_convolve(bottom, sigma)
+	##get plotting stuff
+	x = np.linspace(0,np.ceil(duration/60.0), top.size)
+	mx = max(top.max(), bottom.max())
+	mn = min(top.min(), bottom.min())
+	fig = plt.figure()
+	gs = gridspec.GridSpec(2,2)
+	ax = fig.add_subplot(gs[0,:])
+	ax2 = fig.add_subplot(gs[1,0])
+	ax3 = fig.add_subplot(gs[1,1], sharey=ax2)
+	##the switch points
+	ax.vlines(top_rewarded, mn, mx, colors = 'r', linestyles = 'dashed', 
+		linewidth = '2', alpha = 0.5, label = "top rewarded")
+	ax.vlines(bottom_rewarded, mn, mx, colors = 'b', linestyles = 'dashed', 
+		linewidth = '2', alpha = 0.5, label = "bottom rewarded")
+	ax2.vlines(top_rewarded, mn, mx, colors = 'r', linestyles = 'dashed', 
+		linewidth = '2', alpha = 0.5, label = "top rewarded")
+	ax2.vlines(bottom_rewarded, mn, mx, colors = 'b', linestyles = 'dashed', 
+		linewidth = '2', alpha = 0.5, label = "bottom rewarded")
+	ax3.vlines(top_rewarded, mn, mx, colors = 'r', linestyles = 'dashed', 
+		linewidth = '2', alpha = 0.5, label = "top rewarded")
+	ax3.vlines(bottom_rewarded, mn, mx, colors = 'b', linestyles = 'dashed', 
+		linewidth = '2', alpha = 0.5, label = "bottom rewarded")
+	ax.plot(x, top, color = 'r', linewidth = 2, label = "top lever")
+	ax.plot(x, bottom, color = 'b', linewidth = 2, label = "bottom_lever")
+	ax.legend()
+	ax.set_ylabel("press rate", fontsize = 14)
+	fig.suptitle("Lever press performance", fontsize = 18)
+	ax.set_xlim(-1, x[-1]+1)
+	for tick in ax.xaxis.get_major_ticks():
+		tick.label.set_fontsize(14)
+	for tick in ax.yaxis.get_major_ticks():
+		tick.label.set_fontsize(14)
+	##plot them separately
+	# ##figure out the order of lever setting to create color spans
+	# if top_rewarded.min() < bottom_rewarded.min():
+	# 	for i in range(top_rewarded.size):
+	# 		try:
+	# 			ax2.axvspan(top_rewarded[i], bottom_rewarded[i], facecolor = 'r', alpha = 0.2)
+	# 		except IndexError:
+	# 			ax2.axvspan(top_rewarded[i], duration, facecolor = 'r', alpha = 0.2)
+	# else:
+	# 	for i in range(1,2):
+	# 		try:
+	# 			ax3.axvspan(bottom_rewarded[i], top_rewarded[i], facecolor = 'b', alpha = 0.2)
+	# 		except IndexError:
+	# 			ax3.axvspan(bottom_rewarded[i], duration, facecolor = 'b', alpha = 0.2)
+	ax2.plot(x, top, color = 'r', linewidth = 2, label = "top lever")
+	ax3.plot(x, bottom, color = 'b', linewidth = 2, label = "bottom_lever")
+	ax2.set_ylabel("press rate", fontsize = 14)
+	ax2.set_xlabel("Time in session, mins", fontsize = 14)
+	ax3.set_xlabel("Time in session, mins", fontsize = 14)
+	fig.suptitle("Lever press performance", fontsize = 18)
+	ax2.set_xlim(-1, x[-1]+1)
+	ax3.set_xlim(-1, x[-1]+1)
+	for tick in ax2.xaxis.get_major_ticks():
+		tick.label.set_fontsize(14)
+	for tick in ax2.yaxis.get_major_ticks():
+		tick.label.set_fontsize(14)
+	for tick in ax3.xaxis.get_major_ticks():
+		tick.label.set_fontsize(14)
+	for tick in ax3.yaxis.get_major_ticks():
+		tick.label.set_fontsize(14)
+	ax2.set_title("top only", fontsize = 14)
+	ax3.set_title("bottom only", fontsize = 14)
+
+
+
+
+
+
+
