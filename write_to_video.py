e8ed9463cddd3cb5f00309b5d52f5bfc3a943d87
Ryan Neely <ryanneely11@gmail.com>
2016-08-10 22:44:13 +0000

    Everything in the video writer is workinggit add -u

--- write_to_video.py
+++ write_to_video.py
@@ -6,6 +6,7 @@ import argparse
 import imutils
 import time
 import cv2
+import RPi.GPIO as pi
 
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
@@ -15,13 +16,41 @@ ap.add_argument("-f", "--fps", type=int, default=32,
 	help="FPS of output video")
 ap.add_argument("-c", "--codec", type=str, default="MJPG",
 	help="codec of output video")
+ap.add_argument("-a", "--animal", type=str, default="AnimalX",
+	help="name of animal being recorded")
+ap.add_argument("-s", "--session", type=str, default="Day0",
+	help="Name of recording session")
 args = vars(ap.parse_args())
 
+##global vars for I/O ports
+SESSION_START = 18
+NEW_TRIAL = 19
+
+##set up IO
+print("[INFO] initializing I/O")
+pi.setmode(pi.BCM)
+pi.setup(SESSION_START, pi.IN)
+pi.setup(NEW_TRIAL, pi.OUT)
+trial_status = pi.input(NEW_TRIAL)
+trial_number = 0
+
+##function to catch the rising edge of a new trial signal
+def check_trial():
+        global trial_status
+        global trial_number
+        current_status = pi.input(NEW_TRIAL)
+        if current_status != trial_status and current_status == True:
+                trial_number +=1
+        trial_status = current_status
+                
+                
 # initialize the video stream and allow the camera
 # sensor to warmup
 print("[INFO] warming up cameras...")
-vs1 = VideoStream(src=0, usePiCamera=False, resolution=(1080,720)).start()
-vs2 = VideoStream(src=1, usePiCamera=False, resolution=(1080,720)).start()
+vs1 = VideoStream(src=0, usePiCamera=False, resolution=(800,600),
+                  framerate=args["fps"]).start()
+vs2 = VideoStream(src=1, usePiCamera=False, resolution=(800,600),
+                  framerate=args["fps"]).start()
 time.sleep(2.0)
 
 # initialize the FourCC, video writer, dimensions of the frame
@@ -29,21 +58,33 @@ fourcc = cv2.VideoWriter_fourcc(*args["codec"])
 writer = None
 (h, w) = (None, None)
 
-# loop over frames from the video stream
-while True:
-	# grab the frame from the video streams
-	frame1 = vs.read()
-	frame2 = vs.read()
+animal_name = args["animal"]
+session_name = args["session"]
 
-	cv2.putText(frame1, "Animal_name", (5, 20),cv2.FONT_HERSHEY_PLAIN, 1, (255,255,0))
-	cv2.putText(frame1, "Session_number", (200, 20), cv2.FONT_HERSHEY_PLAIN, 2, (0, 255, 0))
-	cv2.putText(frame1, "Trial_number", (5, 40), cv2.FONT_HERSHEY_PLAIN, 1, (255,255,0))
+##wait for start trigger
+print("[INFO] waiting on start trigger...")
+while not pi.input(SESSION_START):
+        time.sleep(0.1)
+print("[INFO] recording started!")
+
+# loop over frames from the video stream
+while pi.input(SESSION_START):
+	# grab the frame from the video streams
+	frame1 = vs1.read()
+	frame2 = vs2.read()
+
+	##check for a new trial
+	check_trial()
+
+	cv2.putText(frame1, animal_name, (5, 20),cv2.FONT_HERSHEY_PLAIN, 1.5, (255,255,0))
+	cv2.putText(frame1, "Trial# "+str(trial_number), (200, 20), cv2.FONT_HERSHEY_PLAIN, 2, (0, 255, 0))
+	cv2.putText(frame1, session_name, (5, 40), cv2.FONT_HERSHEY_PLAIN, 1.5, (255,255,0))
 
 	# check if the writer is None
 	if writer is None:
 		# store the image dimensions, initialzie the video writer,
 		# and construct the zeros array
-		(h, w) = frame.shape[:2]
+		(h, w) = frame1.shape[:2]
 		writer = cv2.VideoWriter(args["output"], fourcc, args["fps"],
 			(w, h*2), True)
 		zeros = np.zeros((h, w), dtype="uint8")
@@ -52,7 +93,7 @@ while True:
 	# at the top-left, the red channel in the top-right, the green
 	# channel in the bottom-right, and the blue channel in the
 	# bottom-left
-	output = np.zeros((h * 2, w * 2, 3), dtype="uint8")
+	output = np.zeros((h * 2, w, 3), dtype="uint8")
 	output[0:h, 0:w] = frame1
 	output[h:h * 2, 0:w] = frame2
 
@@ -68,7 +109,9 @@ while True:
 		break
 
 # do a bit of cleanup
+print("[INFO] Detected end of recording signal")
 print("[INFO] cleaning up...")
 cv2.destroyAllWindows()
-vs.stop()
+vs1.stop()
+vs2.stop()
 writer.release()
